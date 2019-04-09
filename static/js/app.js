dae87daeea96f0dbadd5faa206fb20ac2b426c6d
FRANCIS AGBODJI <frank@digitalfrank.com>
2019-04-09 19:52:08 -0400

    game business logic 1

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,11 @@
 
-let userId = "";  // Django curent user
+// let userId = "";  // Django curent user
 let secretWord = "";
-let wordReveal = []
-let userWonGame = False
-let missedGuesses = []
+let wordReveal = "";
+let userWonGame = False;
+let missedGuesses = [];
 // these variables are what I save to the WordGame model class and save() into the database after each game
 // this after creating a new instance of WordGame
-
 
 
 function  resetReveal() {
@@ -26,11 +25,12 @@ function fetchWord() {
         success: function(text) {
             secretWord = text;
             for (let i = 0; i < secretWord.length; i++) {
-                wordReveal.push("_ ")
-                if (i = wordReveal.length -1) {
-                    wordReveal.push("_")
-                }
+                wordReveal += "_"
+                // if (i = wordReveal.length -1) {
+                //     wordReveal.push("_")
+                // }
             }
+            $('.revealWord').innerhtml = wordReveal;
         },
         failure: function() {
             alert('connection to API failed, please try again');
@@ -39,27 +39,33 @@ function fetchWord() {
 };
 
 
-function guess(letterInput) {
+function letterGuess(letterInput) {
     for (let i =0; i < secretWord.length; i++) {
         if (secretWord[i] == letterInput) {
             alert("good guess!")
-            wordReveal[i] = letterInput
+            wordReveal.charAt(i) = letterInput
         } else {
             alert(uppercase(letterInput) + " is not in the secret word!");
             missedGuesses.push(letterInput);
             if (missedGuesses.length == 1) {
                 // FrontEnd noose animation
-            } else if (missedGuesses.length == 6) {
+                console.log(missedGuesses)
+            } else if (missedGuesses.length == 2) {
                 // FrontEnd head animation
-            } else if (missedGuesses.length == 6) {
+                console.log(missedGuesses)
+            } else if (missedGuesses.length == 3) {
                 // FrontEnd torso animation
-            } else if (missedGuesses.length == 6) {
+                console.log(missedGuesses)
+            } else if (missedGuesses.length == 4) {
                 // FrontEnd arms animation
-            } else if (missedGuesses.length == 6) {
-                // FrontEnd left leg animation;
+                console.log(missedGuesses)
+            } else if (missedGuesses.length == 5) {
+                // FrontEnd left leg animation
+                console.log(missedGuesses)
             } else if (missedGuesses.length == 6) {
                 // FrontEnd right leg animation
-                alert("Sorry you've lost, try a new word")
+                console.log(missedGuesses)
+                // alert("Sorry you've lost, try a new word")  // done in Django template file
                 // FrontEnd button choice start new game / logout
             };
         };
@@ -68,18 +74,28 @@ function guess(letterInput) {
 
 
 function showMisses() {
-
-}
-
-
-funciton guessWholeWord(wordInput) {
-
-}
+    for (let i =0; i < missedGuesses.length; i++) {
+        missedGuesses.push(missedGuesses[i]);
+        $('#missesList').append(createElement('li').innerhtml(missedGuesses[i]));
+    };
+};
 
 
-function startGame() {
+function wordGuess(wordInput) {
+    if (wordInput == secretWord) {
+        userWonGame = True;
+    } else {
+        alert(wordInput + 'is not the secret word!');
+        missedGuesses.push('*');
+        $('#missesList').append(createElement('li').innerhtml('*'));
+        // FrontEnd hangman animation 
+    };         
+};
+
+
+// function startGame() {
     
-}
+// }
 
 
 
