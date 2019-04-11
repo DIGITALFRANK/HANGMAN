d0c009b068e919c5fb112c06c128228e5db103d7
FRANCIS AGBODJI <frank@digitalfrank.com>
2019-04-11 14:44:01 -0400

    user Auth complete

--- game/models.py
+++ game/models.py
@@ -6,8 +6,10 @@ from django.contrib.auth.models import User
 
 class UserProfileInfo(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    portfolio_site = models.URLField(blank=True)
+    # portfolio_site = models.URLField(blank=True)
     profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
+    badgePoints = models.IntegerField
+    gender = models.CharField(max_length=6, default='')
     def __str__(self):
         return self.user.username
 
